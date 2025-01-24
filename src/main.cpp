72606a8771dfbbda35a54747ebdbeecc16ce3208
Ruohan Wang <ruohanwang20@gmail.com>
2025-01-24 18:07:20 +0100

    add comments

--- src/main.cpp
+++ src/main.cpp
@@ -7,23 +7,18 @@
 #define AS5047P_CHIP_SELECT_PORT 10
 #define AS5047P_CUSTOM_SPI_BUS_SPEED 100000
 #define DEGREES_PER_REVOLUTION 360
-#define PRESCALER 64
 #define SAMPLING_FREQUENCY 250
-#define PID_SAMPLING_FREQUENCY 25
+#define PID_SAMPLING_FREQUENCY 25   // speed pid controller works per 40ms
 
 #define STOP 0
 #define FORWARD 1
 #define BACKWARD 2
 
-#define STRAIGHT 0
-#define LEFT 1
-#define RIGHT 2
-
 #define MAX_SERVO_POSITION 150
 #define MIN_SERVO_POSITION 60
 #define CENTER_SERVO_POSITION 111
 
-#define DISTANCE_PER_REVOLUTION 0.02353
+#define DISTANCE_PER_REVOLUTION 0.02353   // 0.02353m per rotation of the motor
 
 const uint8_t L_EN = 3;
 const uint8_t R_EN = 4;
@@ -31,14 +26,12 @@ const uint8_t L_PWM = 6;
 const uint8_t R_PWM = 5;
 const uint8_t servo_signal = 9;
 
-float pidI = 0;
-float prevError = 0;
-const float pidMin = -60; // Minimum PID output
-const float pidMax = 60;  // Maximum PID output
+const float pidMin = -64; // Minimum PID output
+const float pidMax = 64;  // Maximum PID output
 int outputSpeed = 0;
 
 const float kp = 0.1; // Proportional gain
-const float ki = 0.5; // Integral gain (why integral gain higher than proportional gain? not very common)
+const float ki = 0.5; // Integral gain
 const float kd = 0;   // Derivative gain
 
 const int output_bits = 8;
@@ -56,8 +49,8 @@ Servo steering;
 
 struct Command // The structure of the command read from the serial monitor
 {
-  int mode;
-  int speed;
+  int mode;         // forward, backward, stop
+  int speed;        // speed of the car, from 0.3 to 1m/s
   float distance;
   float direction;
 };
@@ -71,16 +64,11 @@ void stop();
 void steer(float direction);
 
 void passDistance(float currentAngle, float prevAngle);
-// void setupTimer();
 void parseCommand(String input);
 
 void setup()
 {
-  // setupTimer();
-
   steering.attach(servo_signal);
-
-  // sei(); // Enable global interrupts
 
   Serial.begin(115200);
 
@@ -117,15 +105,15 @@ void loop()
   float currentAngle = as5047p.readAngleDegree();
   unsigned long currentTime = micros();
 
-  float deltaAngle = handleRollover(currentAngle - prevAngle);
+  float deltaAngle = handleRollover(currentAngle - prevAngle);  // calculate the delta angle of 2 loops
   long deltaTime = currentTime - prevTime;
 
-  float currentSpeed = calculateCurrentSpeed(deltaAngle, deltaTime);
+  float currentSpeed = calculateCurrentSpeed(deltaAngle, deltaTime);  // calculate the current setpoint of the motor
 
+// speed pid controller works per 40ms
   if (currentTime - pidSamplingTime >= 1000000 / PID_SAMPLING_FREQUENCY)
   {
-    // the output is integer why to cast the output again to integer?
-    outputSpeed = abs(constrain(pid_motor.step(command.speed, currentSpeed), -60, 60));
+    outputSpeed = abs(constrain(pid_motor.step(command.speed, currentSpeed), pidMin, pidMax));
     pidSamplingTime = currentTime;
   }
 
@@ -143,6 +131,7 @@ void loop()
 
     runMotor(command.mode, outputSpeed);
 
+    // if command.distance>0, command.distance decreases one revolution when the motor angle surpasses 360 or 0 degree
     if (command.distance > 0)
     {
       passDistance(currentAngle, prevAngle);
@@ -155,6 +144,7 @@ void loop()
   prevTime = currentTime;
 }
 
+// convert delta angle to the range of (-180, 180)
 float handleRollover(float deltaAngle)
 {
   if (deltaAngle < -DEGREES_PER_REVOLUTION / 2)
@@ -169,13 +159,15 @@ float handleRollover(float deltaAngle)
   return deltaAngle;
 }
 
+// calculate the current setpoint of the motor by delta angle and delta time
 float calculateCurrentSpeed(float deltaAngle, unsigned long deltaTime)
 {
   float rpm = (deltaAngle / deltaTime / 6) * 1000000;
-  float current = 0.025 * rpm; // test
+  float current = 0.025 * rpm; // 0.025 * rpm = setpoint of the motor
   return current;
 }
 
+// stop the motor
 void stop()
 {
   motorController.Stop();
@@ -183,6 +175,7 @@ void stop()
   prevMode = command.mode;
 }
 
+// run the motor with the given mode and signal
 void runMotor(int mode, int signal)
 {
   if (mode == FORWARD)
@@ -195,6 +188,7 @@ void runMotor(int mode, int signal)
   }
 }
 
+// map the command direction to the servo signal(from 60 to 150 degree) and steer the car
 void steer(float direction)
 {
   float steerSignal;
@@ -209,10 +203,10 @@ void steer(float direction)
   steering.write((int)steerSignal);
 }
 
-// why to use 2 arguments, isn't it possible to implement it with delta angle only
+// calculate the distance of the car based on the angle of the motor, if the distance is less than 0, stop the car
 void passDistance(float currentAngle, float prevAngle)
 {
-  // If the angle of the motor surpasses 360 or 0 degree, then distance decreases
+  // If the angle of the motor surpasses 360 or 0 degree, then distance decreases one revolution
   if ((prevAngle > 300 && currentAngle < 60) || (prevAngle < 60 && currentAngle > 300))
   {
     command.distance -= DISTANCE_PER_REVOLUTION; // For one rotation of the motor, the car moves 0.02353m
@@ -225,26 +219,12 @@ void passDistance(float currentAngle, float prevAngle)
   }
 }
 
-// void setupTimer()
-// {
-//   TCCR1A = 0; // Set entire TCCR1A register to 0
-//   TCCR1B = 0; // Same for TCCR1B
-//   TCNT1 = 0;  // Initialize counter value to 0
-
-//   OCR1A = SAMPLING_FREQUENCY; // Set the compare match register
-
-//   // Set prescaler and mode
-//   TCCR1B |= (1 << WGM12);              // CTC mode (Clear Timer on Compare)
-//   TCCR1B |= (1 << CS11) | (1 << CS10); // Set prescaler to 64
-
-//   // Enable timer compare interrupt
-//   TIMSK1 |= (1 << OCIE1A);
-// }
-
+// parse the command read from the serial monitor, and set the command structure
+// if speed>0, set command.mode=FORWARD, if speed<0, set command.mode=BACKWARD, if speed=0, set command.mode=STOP
 void parseCommand(String input)
 {
   // Split string based on commas
-  Serial.println(input);
+  // Serial.println(input);
   int speedIndex = input.indexOf("speed:");
   int distanceIndex = input.indexOf("distance:");
   int directionIndex = input.indexOf("direction:");
@@ -265,7 +245,7 @@ void parseCommand(String input)
     {
       command.mode = STOP;
     }
-    command.speed = constrain(command.speed, -64, 64); // the speed of motor should be in the range of (-64, 64)
+    command.speed = constrain(command.speed, pidMin, pidMax); // the setpoint of motor should be in the range of (-64, 64), 64 is 1m/s
   }
 
   if (distanceIndex != -1)
